fcd87a54e578c14512918256cc3b7ad5a02fa9a1
Erol Kaftanoglu <erolkaftanoglu@gmail.com>
2021-07-10 14:13:56 +0300

    RecyclerView with retrofit

--- app/src/main/java/com/kodluyoruz/networkandretrofit/MainActivity.kt
+++ app/src/main/java/com/kodluyoruz/networkandretrofit/MainActivity.kt
@@ -1,27 +1,40 @@
 package com.kodluyoruz.networkandretrofit
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.kodluyoruz.networkandretrofit.models.listing.RickAndMortyBaseResponse
 
 class MainActivity : AppCompatActivity() {
+    private val retrofitHelper = RetrofitHelper()
+    private val adapter: RickMortyAdapter = RickMortyAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initViews()
+        fetchData()
+    }
 
-        val retrofitHelper = RetrofitHelper()
-        retrofitHelper.listCharacter(page = 1, object: RetrofitResponseHandler {
+    private fun fetchData() {
+        retrofitHelper.listCharacter(page = 1, object : RetrofitResponseHandler {
             override fun onError() {
                 Log.v("MainActivity", "Error :(")
 
             }
 
             override fun onResponse(response: RickAndMortyBaseResponse) {
-                response.characters.forEach { character ->
-                    Log.v("MainActivity", "Character: ${character.name}")
-                }
+                adapter.setRickMortyData(response.characters)
             }
         })
+    }
+
+    private fun initViews() {
+        val recyclerView: RecyclerView = findViewById(R.id.rickAndMortyRecyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(baseContext)
+        recyclerView.adapter = adapter
+
     }
 }
---
+++ app/src/main/java/com/kodluyoruz/networkandretrofit/RickMortyAdapter.kt
@@ -0,0 +1,48 @@
+package com.kodluyoruz.networkandretrofit
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.kodluyoruz.networkandretrofit.models.core.Character
+
+class RickMortyAdapter : RecyclerView.Adapter<RickMortyAdapter.RickMortyViewHolder>() {
+
+    var list = ArrayList<Character>()
+
+    class RickMortyViewHolder(item: View) : RecyclerView.ViewHolder(item) {
+        private val rickAndMortImageView: ImageView = item.findViewById(R.id.chacterImageView)
+        private val nameTextView: TextView = item.findViewById(R.id.nameTextView)
+        private val isAliveTextView: TextView = item.findViewById(R.id.isAliveTextView)
+        fun bind(character: Character) {
+            Glide.with(rickAndMortImageView.context)
+                .load(character.image).into(rickAndMortImageView);
+            nameTextView.text = character.name
+            isAliveTextView.text = character.status
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RickMortyViewHolder {
+        val viewItem =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_rickmorty, parent, false)
+        return RickMortyViewHolder(viewItem)
+    }
+
+    override fun onBindViewHolder(holder: RickMortyViewHolder, position: Int) {
+        val item = list[position]
+        holder.bind(item)
+    }
+
+    override fun getItemCount(): Int = list.size
+
+    fun setRickMortyData(list: List<Character>) {
+        this.list = ArrayList(list)
+        notifyDataSetChanged()
+    }
+
+
+
+}
